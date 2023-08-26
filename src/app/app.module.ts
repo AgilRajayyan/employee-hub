9a5a3dd1211c1e2bfeaf7b3dd7232e8719d7055b
Agil Rajayyan <66911936+AgilRajayyan@users.noreply.github.com>
2023-08-26 18:43:36 +0530

    -Added employee-form and employee-list components and configured their routes in app-routing module -Added navbar (using MatToolbar) and the navbar title gets updated based on the route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DBConfig, NgxIndexedDBModule } from 'ngx-indexed-db';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeeFormComponent } from './components/employee-form/employee-form.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const dbConfig: DBConfig = {
   name: 'employeesDB',
@@ -32,12 +34,13 @@ const dbConfig: DBConfig = {
 };
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, EmployeeFormComponent, EmployeeListComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     NgxIndexedDBModule.forRoot(dbConfig),
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
